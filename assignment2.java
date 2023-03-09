5a22e1958161471553fe94c81b358ac731c1c3dc
896246 <126397754+896246@users.noreply.github.com>
2023-03-10 01:49:36 +0530

    Add files via upload

---
+++ assignment2.java
@@ -0,0 +1,34 @@
+Answere 1
+ Statically typed language means the memory to the variable is assigned  after the compilation.
+ Dynamically typed language means the memory to the variable is assigned during  execution.
+
+Answere 2
+ Variable is a just a conatiner which is used to hold the value that value can be change. Or variable is reserved memory location.
+ type variablename = value;
+
+
+Answere 3
+ We can assign the value to the variable using assignment operator.
+
+Answere 4
+ Primmitive data types 
+ byte
+ boolean
+ short
+ cahr
+ int 
+ long
+ float
+ double
+
+Answere 4
+ Identifier is a name which is given to class, method, package, variable name.
+ Rules for the identifier
+ 1. Identifier can be start with ([a-zA-Z] ,$, _) 
+ 2. Identifier can not be start with digit.
+ 3. Identifier can not contain white space.
+ 4. Identifier can not contain Keyword.
+
+
+Answere 5
+ Uanary,Arithmetic, Shift, Relational, BIt Wise, Logical, Ternary, Assignment, Comma.
