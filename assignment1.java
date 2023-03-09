fee518767f4f013490f221bd0203a7c2e0f00764
896246 <126397754+896246@users.noreply.github.com>
2023-03-10 01:18:20 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,17 @@
+Answere 1 
+  Programming language is a language which is going to help to the programmer to communicate with the computer.
+
+Answere 2
+ We need a programming language because we can not communicate with the computer in the form of binary language.
+
+Answere 3
+ platform independant, opps oriented, secure, roboust.
+
+Answere 4
+ Object is real word entity or  instance of the class.We can create an object using new keyword.
+
+Answere 5
+ Class is a blueprint of template or logical entity which is used to represent real word entity.
+
+Answere 6
+ Main is the method from where JVM starts the excecution of the java programme.
