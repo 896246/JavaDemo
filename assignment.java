eedc43cf751361efd4c2a318a03cc77c22b04fc8
896246 <126397754+896246@users.noreply.github.com>
2023-03-10 00:58:41 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,31 @@
+Answere 1
+    Computer is a electronic  device which is use to take the input from the user and produces the output.
+    eg. when we perform the addition of the two number that two number act as input for the computer and after performing the operation it produced the result.
+
+Answere 2
+   RAM is a volatile memory in the computer system. It  is used to provide the type compatibility for the CPU  because cpu have a semiconductor nature and ram is also 
+   has semiconductor nature so taking the data from the secondary memory CPU tackes the data from the Primary memory.
+
+
+Answere 3
+   Data is stored into the various place of the computer.
+    if data is stored in the form of Byte that means stored into the primary momory(RAM).
+    if data is stored in the form of file that means stored into the secondary momory(Hard Disk).
+    if data is stored in the form of register that means stored into the CPU(Heart of The computer).
+    
+
+Answere 4
+  keyboard, joystick, mouse.
+
+Answere 5
+  monitor.
+
+Answere 5
+ mouse
+
+
+Answere 6
+ machine level language,binary language.
+
+
+ 
